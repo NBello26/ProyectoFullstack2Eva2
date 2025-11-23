dd2eddeb6c5ce548620d7e6df9ab7b6aa72940c6
Nicolas <Nicolas.vicente1226@gmail.com>
2025-11-23 00:03:18 -0300

    Agregar import link

--- src/componentes/organismos/MainProductos.jsx
+++ src/componentes/organismos/MainProductos.jsx
@@ -1,4 +1,5 @@
 import React, { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
 import TablaProductos from "../moleculas/TablaProductos.jsx";
 import "../../estilos/mainProductosPage.css";
 const API_URL = process.env.REACT_APP_API_URL;
